f80e03c52a71ac1f30d019ee365d79889d148363
Jawad <jawadakhter7@gmail.com>
2024-07-10 12:40:29 +0500

    updated mongodb localhost to online cluster because it was not working on vercel

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -2,7 +2,7 @@
 
 import { MongoClient } from 'mongodb';
 
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb://jawadakhter7:wxniB4aK9PAYJR5Y@ac-uqql9vi-shard-00-00.wrxrpup.mongodb.net:27017,ac-uqql9vi-shard-00-01.wrxrpup.mongodb.net:27017,ac-uqql9vi-shard-00-02.wrxrpup.mongodb.net:27017/?replicaSet=atlas-vmcs2r-shard-0&ssl=true&authSource=admin';
 const options = {};
 
 let client;
